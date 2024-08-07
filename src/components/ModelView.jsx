08e7b116eb2a51c893b9415ec24d1de8ec0321d9
Matheus <mmufato@gmail.com>
2024-08-07 00:45:40 -0300

    Model done and ModelView started

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,11 @@
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+    
+    return (
+        <view
+            index={index}>
+
+        </view>
+    )
+}
+
+export default ModelView
