7eb22b5e3f775c6e6c9d44143c9ec0ded48e13cc
Matheus <mmufato@gmail.com>
2024-08-07 12:18:36 -0300

    Add 3d model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,12 @@
-import { View  } from '@react-three/drei'
+import { OrbitControls, View  } from '@react-three/drei'
 import Lights from './Lights'
 import { Suspense } from 'react'
 import Iphone from './Iphone'
+import * as THREE from 'three'
+import Loader from './Loader'
 
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationStated, size, item}) => {
     
     return (
         <View
@@ -19,10 +21,30 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
 
             <Lights />
 
-            <Suspense fallback={<div>Loading</div>}>
-                <Iphone/>
+            <OrbitControls
             
-            </Suspense>
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+                onEnd={() => setRotationStated(controlRef.current.getAzimuthalAngle())}
+            
+            />
+
+            <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0, 0, 0]}>
+                <Suspense fallback={<Loader />}>
+                    <Iphone
+                        scale={index === 1 ? [30, 30, 30] : [33, 33, 33]}
+                        item={item}
+                        size={size}
+                    />
+            
+                </Suspense> 
+            </group>
+
+            
             
         </View>
     )
